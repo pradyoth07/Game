8f85f4aa417423cacca76e3db70797a3a45f8d1d
pradyoth07 <35808129+pradyoth07@users.noreply.github.com>
2018-03-25 15:51:56 -0400

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { LoopComponent } from './loop/loop.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    MessagesComponent,
+    HomeComponent,
+    GameComponent,
+    LoopComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+        { path: 'home', component: HomeComponent },
+        { path: 'game', component: GameComponent },
+        { path: '', redirectTo: '/home', pathMatch: 'full'}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/loop/loop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loop',
+  templateUrl: './loop.component.html',
+  styleUrls: ['./loop.component.css']
+})
+export class LoopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
